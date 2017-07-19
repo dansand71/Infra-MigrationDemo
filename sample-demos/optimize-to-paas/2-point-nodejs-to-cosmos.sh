36fdeb2b22925e24808722f1b424d37afab6f451
dansand71 <dansand71@hotmail.com>
2017-07-19 09:30:37 -0700

    demo update

--- sample-demos/optimize-to-paas/2-point-nodejs-to-cosmos.sh
+++ sample-demos/optimize-to-paas/2-point-nodejs-to-cosmos.sh
@@ -7,10 +7,14 @@ echo "SSH into Node Frontend and set variables"
 echo "  replace: dansand-mongo-svr1:27017/todo"
 echo "  with the new cosmosdb connection"
 
-ssh -i ~/Desktop/msready-demo/aws-linux-vms.pem centos@azure-node-todo bash <<EOF
+ssh -i ~/Desktop/msready-demo/aws-linux-vms.pem centos@dansand-az-nodetodo.eastus.cloudapp.azure.com bash <<EOF
     sudo su
+
+    #CHANGE the db connection string
     sed -i 's|NODE_TODO_MONGO_DBCONNECTION.*|NODE_TODO_MONGO_DBCONNECTION: '\''mongodb://nodejs-todo:VbtkysT46KJx2FcTECQFVCVtOK4EWCTWTM9zwcEA55V3sgF1onpeQrUqPwXTfD1ufwLGrkNTmxLtMA75wyMVmg==@nodejs-todo.documents.azure.com:10255/todo?ssl=true'\''|g' /source/nodejs-todo/ecosystem.config.js
-    sed -i 's|NODE_DB_Type.*|NODE_DB_Type: '\''cosmosdb mongo instance'\''|g' /source/nodejs-todo/ecosystem.config.js
+    sed -i 's|NODE_DB_Type.*|NODE_DB_Type: '\''cosmosdb mongo instance'\'',|g' /source/nodejs-todo/ecosystem.config.js
+    
+    #RESTART PM2 and bring up the NODE app
     cd /source/nodejs-todo
     pm2 delete nodejs-todo
     pm2 save
--- sample-demos/optimize-to-paas/3-move-nodejs-to-containers.sh
+++ sample-demos/optimize-to-paas/3-move-nodejs-to-containers.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
 #Containerize the nodejs app & push to Azure CR
-ssh -i ~/Desktop/msready-demo/aws-linux-vms.pem centos@azure-node-todo bash <<EOF
+ssh -i ~/Desktop/msready-demo/aws-linux-vms.pem centos@dansand-az-nodetodo.eastus.cloudapp.azure.com bash <<EOF
     sudo su
     cd /source/nodejs-todo
+    
+    #BUILD the new container
     docker build -t ossdemo/nodejs-todo .
     
-    #docker tag ossdemo/nodejs-todo VALUEOF-REGISTRY-SERVER-NAME/ossdemo/nodejs-todo
+    #TAG the Image
     docker tag ossdemo/nodejs-todo dansandossdemoinfra.azurecr.io/ossdemo/nodejs-todo
     
-    #docker login VALUEOF-REGISTRY-SERVER-NAME -u VALUEOF-REGISTRY-USER-NAME -p VALUEOF-REGISTRY-PASSWORD
+    #LOGIN to the Azure Container Registry
     docker login dansandossdemoinfra.azurecr.io -u dansandossdemoinfra -p /fQ+paow/+udNO/d6Lnf=CP4InxZrcJO
     
-    #docker push VALUEOF-REGISTRY-SERVER-NAME/ossdemo/nodejs-todo
+    #PUSH the new image up
     docker push dansandossdemoinfra.azurecr.io/ossdemo/nodejs-todo
 
-    echo ".updating the web app with the container details"
+    
 EOF
 ## Config the Docker Container
 echo ""
-echo ".setting the docker registry settings"
+echo ".updating the web app with the container details"
+echo ".setting the docker registry settings on the webapp"
     ~/bin/az webapp config container set -n dansandinfra-nodejs-todo -g ossdemo-infra-migrate \
         --docker-registry-server-password /fQ+paow/+udNO/d6Lnf=CP4InxZrcJO \
         --docker-registry-server-user dansandossdemoinfra \
         --docker-registry-server-url dansandossdemoinfra.azurecr.io \
         --docker-custom-image-name dansandossdemoinfra.azurecr.io/ossdemo/nodejs-todo 
 
-echo ".setting the MONGODB Connection for the Container service..."    
+echo ".setting the MONGODB Connection for the container..."    
 ## Config the Mongo DB Connection
     ~/bin/az webapp config appsettings set -n dansandinfra-nodejs-todo -g ossdemo-infra-migrate \
         --settings NODE_TODO_MONGO_DBCONNECTION="mongodb://nodejs-todo:VbtkysT46KJx2FcTECQFVCVtOK4EWCTWTM9zwcEA55V3sgF1onpeQrUqPwXTfD1ufwLGrkNTmxLtMA75wyMVmg==@nodejs-todo.documents.azure.com:10255/todo?ssl=true"
